a72dd541b6d142495d611a3aaad1f38d253be0bf
Sharique Mahmood <shariquemahmood@gmail.com>
2025-10-02 17:44:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-815479224-eac21",
+  "appId": "1:278652161771:web:ad74f994ee9f0229d3b8ae",
+  "apiKey": "AIzaSyD_boK7MlIWRscva7q1Jtl_Sg43GulPMGY",
+  "authDomain": "studio-815479224-eac21.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "278652161771"
+};
