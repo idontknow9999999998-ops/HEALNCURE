b2c82401efa1f3b70a5239373bfe0e4f89053455
Sharique Mahmood <shariquemahmood@gmail.com>
2025-10-03 15:49:49 +0000

    “Please update my Firebase config so it uses environment variables inste

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-815479224-eac21",
-  "appId": "1:278652161771:web:ad74f994ee9f0229d3b8ae",
-  "apiKey": "AIzaSyD_boK7MlIWRscva7q1Jtl_Sg43GulPMGY",
-  "authDomain": "studio-815479224-eac21.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "278652161771"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
